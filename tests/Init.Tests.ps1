2f47d1d861c732a8980010a717261b06dc47db11
fawohlsc <fawohlsc@microsoft.com>
2021-04-08 16:31:04 +0200

    Fixed

--- tests/Init.Tests.ps1
+++ tests/Init.Tests.ps1
@@ -1,5 +1,3 @@
-using module "C:\Users\fawohlsc\Repos\azure-policy-testing\utils\TestContext.psm1"
-
 BeforeDiscovery {
     # Suppress breaking change warning in Azure PowerShell
     # See also: https://github.com/Azure/azure-powershell/blob/master/documentation/breaking-changes/breaking-changes-messages-help.md
